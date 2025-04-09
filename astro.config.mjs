c325c7978c41e5692cf9390f37935465bda38ba5
Kazuki Tobita <kazukiti201@gmail.com>
2025-04-10 07:19:49 +0900

    Add GitHub Actions settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://uyupun.github.io',
+	base: '/takada-semi',
 	integrations: [
 		starlight({
 			title: '高田ゼミ',
